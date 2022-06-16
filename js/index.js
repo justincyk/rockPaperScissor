301aa46d6a2c5267b4e8622f738fe240ee261135
Justin Kim <jcykim@csu.fullerton.edu>
2022-06-15 17:57:17 -0700

    Added js code to change DOM elements content after each battle

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function computerPlay()
 {
-  let moves = ["Rock", "Paper", "Scissor"];
+  let moves = ["rock", "paper", "scissor"];
   let index = Math.floor(Math.random()*3);
   return moves[index];
 }
@@ -11,43 +11,87 @@ function playRound(playerSelect, computerSelect)
   let computerSelection = computerSelect.toLowerCase();
   if ( playerSelection === "paper" && computerSelection === "rock" )
   {
-    return "You Win! Paper beats rock.";
+    return "Win";
   }
   else if ( playerSelection === "rock" && computerSelection === "paper" )
   {
-    return "You Lose! Paper beats rock.";
+    return "Lose";
   }
   else if ( playerSelection === "paper" && computerSelection === "scissor" )
   {
-    return "You Lose! Scissors beats paper.";
+    return "Lose";
   }
   else if ( playerSelection === "scissor" && computerSelection === "paper" )
   {
-    return "You Win! Scissors beats paper.";
+    return "Win";
   }
   else if ( playerSelection === "rock" && computerSelection === "scissors" )
   {
-    return "You Win! Rock beats scissors";
+    return "Win";
   }
   else if ( playerSelection === "scissor" && computerSelection === "rock" )
   {
-    return "You Lose! Rock beats scissors";
+    return "Lose";
   }
   else
   {
-    return `It's a Tie! You guys both did ${playerSelection}.`
+    return "Tie";
   }
 }
 
+let moves = document.querySelectorAll(".move");
+let userWins = 0;
+let computerWins = 0;
+let count = 0;
 
-function game()
+moves.forEach( (move) => move.addEventListener('click', (e) =>
 {
-  for( let i = 0; i < 5; ++i )
+  let result = document.querySelector('.result');
+  result.setAttribute('style','white-space: pre;');
+  const computerMove = computerPlay();
+  result.textContent = `Player chose: ${e.target.id} & Computer chose: ${computerMove}.\r\n`;
+  let winner =  playRound(e.target.id, computerMove);
+  result.textContent += winner;
+  ++count;
+  updateGame(winner, count);
+}
+));
+
+
+function updateGame(winner, count){
+  let user = document.querySelector('#user');
+  let computer = document.querySelector('#computer');
+  if( winner === "Tie")
   {
-    let playerSelect = prompt("What move do you want to make?");
-    let computerSelect = computerPlay();
-    console.log(playRound(playerSelect, computerSelect));
+    ++userWins;
+    ++computerWins;
   }
+  else winner === "Win" ? ++userWins : ++computerWins;
+  user.textContent = userWins;
+  computer.textContent = computerWins;
+
+  if ( count  === 5 )
+  {
+    const winner = userWins > computerWins ? "User" : "Computer";
+    let result = document.querySelector('.result');
+    result.textContent = `The game has finished. ${ result === "User" ? "You have won the game!" :
+    "The Computer has won the game!"}`;
+    count = 0;
+    userWins = 0;
+    computerWins = 0;
+  }
+  return;
 }
 
-game();
+
+// function game()
+// {
+//   for( let i = 0; i < 5; ++i )
+//   {
+//     let playerSelect = prompt("What move do you want to make?");
+//     let computerSelect = computerPlay();
+//     console.log(playRound(playerSelect, computerSelect));
+//   }
+// }
+
+// game();
